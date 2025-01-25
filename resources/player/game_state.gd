2ec767ac54855b0c87fc73e4138c7c09ace12000
Jakl <57708725+Jakllp@users.noreply.github.com>
2025-01-25 11:05:44 +0100

    Proper limits

---
+++ resources/player/game_state.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var play_area :Vector2 = Vector2(256, 256):
+	set(value):
+		play_area_update.emit(value)
+		play_area = value
+var play_area_upgrades = [ 256, 384, 512, 768 ]
+
+signal play_area_update(play_area :Vector2)
--- resources/player/player_cam.gd
+++ resources/player/player_cam.gd
@@ -5,18 +5,30 @@ extends Camera2D
 @export var max_zoom = 10
 
 var cur_upgrade = 0
-
 var zoom_upgrades = [ 6, 4, 2, 1 ]
+
+func _ready() -> void:
+	GameState.play_area_update.connect(change_play_area)
+	change_play_area(GameState.play_area)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.is_action_pressed("cam_drag"):
-		position += event.relative * -sens
+		var act_screen_width_half = (get_viewport_rect().size.x * (1/zoom.x))/2
+		var act_screen_height_half = (get_viewport_rect().size.y * (1/zoom.y))/2
+		position.x = clamp(position.x + event.relative.x * -sens, -GameState.play_area.x + act_screen_width_half, GameState.play_area.x - act_screen_width_half)
+		position.y = clamp(position.y + event.relative.y * -sens, -GameState.play_area.y + act_screen_width_half, GameState.play_area.y - act_screen_width_half)
 	if event.is_action_pressed("reset_cam"):
 		position = Vector2.ZERO
 	if event.is_action("zoom_in"):
 		change_zoom(zoom + Vector2(zoom_sens, zoom_sens))
 	if event.is_action("zoom_out"):
 		change_zoom(zoom - Vector2(zoom_sens, zoom_sens))
+
+func change_play_area(play_area :Vector2) -> void:
+	limit_right = play_area.y
+	limit_left = -play_area.y
+	limit_top = -play_area.x
+	limit_bottom = play_area.x
 
 func change_zoom(zoom_level :Vector2) -> void:
 	zoom = clamp(zoom_level, Vector2(zoom_upgrades[cur_upgrade],zoom_upgrades[cur_upgrade]), Vector2(max_zoom,max_zoom))
