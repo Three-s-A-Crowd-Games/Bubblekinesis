cd018ef35191392d9e93977e87817fe9bc9b380a
Jakl <57708725+Jakllp@users.noreply.github.com>
2025-01-25 16:03:40 +0100

    Add important note

--- resources/player/game_state.gd
+++ resources/player/game_state.gd
@@ -4,6 +4,7 @@ var play_area :Vector2 = Vector2(256, 256):
 	set(value):
 		play_area_update.emit(value)
 		play_area = value
+# NOTE: We always HAVE to have one more than actual available upgrades
 var play_area_upgrades = [ 256, 384, 512, 768 ]
 var cur_play_area_upgrade = 0
 
