00b405c19aede33c46d499c2726e8de8a3a10003
Jakl <57708725+Jakllp@users.noreply.github.com>
2025-01-27 14:39:47 +0100

    Fix scaling

--- resources/sub_viewport.gd
+++ resources/sub_viewport.gd
@@ -3,8 +3,9 @@ extends SubViewport
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	size.y = 426 * (1/DisplayServer.screen_get_size().aspect())
-	get_parent().size.y = 1280 * (1/DisplayServer.screen_get_size().aspect())
+	size = DisplayServer.screen_get_size() / 4
+	get_parent().size = DisplayServer.screen_get_size()
+	get_parent().position = get_parent().size / -4
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
